2048c8732582091094935181615001f9c2793704
Ccc1l <610178666@qq.com>
2018-10-23 23:25:24 +0800

    button 基本样式添加

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ### 一个vue UI 组件库 
+### 安装
+#### 使用本框架前，请在 css 中开启 border-box
+```
+box-sizing: border-box
+```
---
+++ button.js
@@ -0,0 +1,5 @@
+Vue.component('ccc1l-button', {
+    template: `
+        <button class="ccc1l-button">行动按钮</button>
+    `
+});
