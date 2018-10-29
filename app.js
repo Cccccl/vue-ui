173644782617764b816b195cc227c6862f42fd51
Ccc1l <610178666@qq.com>
2018-10-30 00:39:11 +0800

    chai 断言库的引入

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import Button from './package/button/index.js'
 import Icon from './package/icon/index.js'
 import ButtonGroup from './package/button-group/index.js'
+
 Vue.component(Button.name, Button);
 Vue.component(Icon.name, Icon);
 Vue.component(ButtonGroup.name, ButtonGroup);
+
 new Vue({
     el: '#root',
     data: function () {
@@ -22,3 +24,5 @@ new Vue({
         }
     }
 });
+
+// 单元测试
